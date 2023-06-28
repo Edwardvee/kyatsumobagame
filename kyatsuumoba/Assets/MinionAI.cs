dc72bf117afefa6954baac68fa9b7ea3dfcf7a16
Edwardvee <lautaroivan.quiroga@gmail.com>
2023-06-27 22:47:47 -0300

    IA basica de los minions

--- kyatsuumoba/Assets/MinionAI.cs
+++ kyatsuumoba/Assets/MinionAI.cs
@@ -6,17 +6,27 @@ using UnityEngine.AI;
 public class MinionAI : MonoBehaviour
 {
     public Vector3 destination;
+    public Vector3 finalDestination;
+
+
     NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        agent.SetDestination(destination);  
+        agent.SetDestination(destination);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(this.transform.position.x >= 19 && this.transform.position.z <= -26){ 
+    agent.SetDestination(finalDestination);
+            }
+        if (this.transform.position.x <= -34 && this.transform.position.z >= 26)
+        {
+            agent.SetDestination(finalDestination);
+        }
     }
 }
