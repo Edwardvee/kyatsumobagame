ccf68b5e3017fd30bc8c9765d23bc8213d9950ab
Edwardvee <lautaroivan.quiroga@gmail.com>
2023-07-05 00:07:59 -0300

    aiuda

--- kyatsuumoba/Assets/scripts/Player/playerHealthBar.cs
+++ kyatsuumoba/Assets/scripts/Player/playerHealthBar.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class playerHealthBar : MonoBehaviour
 {
-   
-
+    public GameObject unit;
+    public Vector2 offSet;
+    Vector2 GetScreenPosition(Vector3 worldPos)
+    {
+        return Camera.main.WorldToScreenPoint(worldPos);
+    }
+    void FollowUnit() {
+        Vector2 pos = GetScreenPosition(unit.transform.position + new Vector3(0f, 2.2f, 0f));
+        pos += offSet;
+        transform.position = pos;   
+    }
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform);
-        transform.Rotate(0, 180, 0);
+        FollowUnit();
+
+
     }
+
+
 }
     
